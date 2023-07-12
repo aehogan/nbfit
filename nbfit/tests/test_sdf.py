a0e5b67284d1d1c1113a014dfba45323cad16b3c
aehogan <purplepanzer1@gmail.com>
2023-07-12 17:21:30 -0400

    Load .sdf files

--- nbfit/tests/test_sdf.py
+++ nbfit/tests/test_sdf.py
@@ -1,2 +1,11 @@
 import pytest
+import glob
+from nbfit.utilities.sdf import load_sdfs
+from openff.toolkit import Molecule
 
+
+def test_methanolx8():
+    filenames = glob.glob("example_sdfs/S66x8/*.sdf")
+    mols = load_sdfs(filenames)
+    for mol in mols:
+        print(Molecule.are_isomorphic(mols[0], mol, return_atom_map=True))
